00c2cf792a1efdc93c14a120d9149a3e3a8ac201
rukednous <luke.winikates@gmail.com>
2013-11-18 19:52:49 -0800

    add watch view

--- app/client/js/routes.js
+++ app/client/js/routes.js
@@ -1,4 +1,3 @@
-Firebase = function(){}
 App.Router.map(function() {
   this.resource('index', {path: '/'})
   this.resource('broadcast', {path: '/broadcast/:id'})
@@ -6,20 +5,52 @@ App.Router.map(function() {
 });
 
 App.BroadcastRoute = Em.Route.extend({
-  //afterModel:function(controller) {
-    //controller.set("frameSource", Leap.loop)
-    //console.log("setupController");
-  //},
   model: function(params, transition) {
     var fb = new Firebase(App.ENV.FIREBASE_URL + '/'+ params.id);
-    return {fb: fb, frameSource: Leap.loop};
+    return {fb: fb, frameSource: new App.LeapFrameSource()};
   }
-})
+});
 
 App.WatchRoute = Em.Route.extend({
   model: function(params, transition) {
     var fb = new Firebase(App.ENV.FIREBASE_URL + '/'+ params.id);
-    return fb;
+    new App.FirebaseFramePusher(fb).start();
+    return {frameSource: new App.FirebaseFrameSource(fb)};
+  }
+});
+
+App.FirebaseFramePusher = Ember.Object.extend({
+  init: function(firebase) {
+    this.firebase = firebase;
+    _.bindAll(this, 'start')
+  },
+  start: function(){
+    var self = this;
+    Leap.loop(function(frame){
+      var pointablesMap = frame.pointablesMap;
+      var points = _.map(pointablesMap, function(p) { return {id: p.id, tipPosition: p.tipPosition}});
+      self.firebase.set({pointablesMap: points});
+    });
   }
 })
 
+App.LeapFrameSource = Ember.Object.extend({
+  subscribe:function(frameCallback) {
+    Leap.loop(frameCallback);
+  },
+  stopListening: function(frameCallback) {
+    Leap.loopController.removeListener(frameCallback);
+  }
+});
+
+App.FirebaseFrameSource = Ember.Object.extend({
+  init: function(firebase) {
+    this.firebase = firebase;
+  },
+  subscribe: function(frameCallback) {
+    this.firebase.on('value', function(f){frameCallback(f.val())});
+  },
+  stopListening: function(frameCallback) {
+    this.firebase.off();
+  }
+});
--- app/client/js/views.js
+++ app/client/js/views.js
@@ -1,8 +1,7 @@
 App.HandVisualizerComponent = Ember.Component.extend({
   didInsertElement: function(){
-    var canvas = document.getElementById("leap-overlay");
     var frameSource = this.get("frameSource")
-
+    var canvas = document.getElementById("leap-overlay");
     // fullscreen
     canvas.width = document.body.clientWidth;
     canvas.height = document.body.clientHeight;
@@ -11,31 +10,32 @@ App.HandVisualizerComponent = Ember.Component.extend({
     var ctx = canvas.getContext("2d");
     ctx.translate(canvas.width/2,canvas.height);
     ctx.fillStyle = "rgba(0,0,0,0.7)";
-
-    // render each frame
-    function draw(obj) {
-      // clear last frame
+    // listen to Leap Motion
+    var renderer = function(frame) {
+    // clear last frame
       ctx.clearRect(-canvas.width/2,-canvas.height,canvas.width,canvas.height);
-
       //A.push(obj);
       // render circles based on pointable positions
-      var pointablesMap = obj.pointablesMap;
+      var pointablesMap = frame.pointablesMap;
       points = _.map(pointablesMap, function(p) { return {id: p.id, tipPosition: p.tipPosition}});
-      model.set(points);
       for (var i in points) {
         // get the pointable's position
         var pointable = points[i]
-        var pos = pointable.tipPosition;
+        if(pointable) {
+          var pos = pointable.tipPosition;
 
-        // create a circle for each pointable
-        var radius = Math.min(600/Math.abs(pos[2]),20);
-        ctx.beginPath();
-        ctx.arc(pos[0]-radius/2,-pos[1]-radius/2,radius,0,2*Math.PI);
-        ctx.fill();
+          // create a circle for each pointable
+          var radius = Math.min(600/Math.abs(pos[2]),20);
+          ctx.beginPath();
+          ctx.arc(pos[0]-radius/2,-pos[1]-radius/2,radius,0,2*Math.PI);
+          ctx.fill();
+        }
       }
-    };
-
-    // listen to Leap Motion
-    Leap.loop(draw);
+    }
+    this.set('renderer', renderer);
+    frameSource.subscribe(renderer);
+  },
+  willDestroyElement: function(){
+    frameSource.stopListening(this.get('renderer'));
   }
 });
